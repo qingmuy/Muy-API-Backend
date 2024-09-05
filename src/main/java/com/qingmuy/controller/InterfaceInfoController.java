274a8b793fa21c45051a12efc45bd98f65480efa
Muy <lzqm04@gmail.com>
2024-09-06 00:04:45 +0800

    接口上下线接口的开发

--- src/main/java/com/qingmuy/controller/InterfaceInfoController.java
+++ src/main/java/com/qingmuy/controller/InterfaceInfoController.java
@@ -3,10 +3,7 @@ package com.qingmuy.controller;
 import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.qingmuy.annotation.AuthCheck;
-import com.qingmuy.common.BaseResponse;
-import com.qingmuy.common.DeleteRequest;
-import com.qingmuy.common.ErrorCode;
-import com.qingmuy.common.ResultUtils;
+import com.qingmuy.common.*;
 import com.qingmuy.exception.BusinessException;
 import com.qingmuy.exception.ThrowUtils;
 import com.qingmuy.model.dto.interfaceinfo.InterfaceInfoAddRequest;
@@ -14,10 +11,13 @@ import com.qingmuy.model.dto.interfaceinfo.InterfaceInfoQueryRequest;
 import com.qingmuy.model.dto.interfaceinfo.InterfaceInfoUpdateRequest;
 import com.qingmuy.model.entity.InterfaceInfo;
 import com.qingmuy.model.entity.User;
+import com.qingmuy.model.enums.InterfaceInfoStatusEnum;
+import com.qingmuy.muyapiclientsdk.client.MuyApiClient;
 import com.qingmuy.service.InterfaceInfoService;
 import com.qingmuy.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -32,6 +32,9 @@ public class InterfaceInfoController {
 
     @Resource
     private InterfaceInfoService interfaceInfoService;
+
+    @Resource
+    MuyApiClient muyApiClient;
 
     /**
      * 创建接口
@@ -119,6 +122,80 @@ public class InterfaceInfoController {
         ThrowUtils.throwIf(interfaceInfo == null, ErrorCode.NOT_FOUND_ERROR);
         interfaceInfoService.removeById(deleteRequest.getId());
         return ResultUtils.success("删除成功");
+    }
+
+    /**
+     * 发布接口
+     *
+     * @param idRequest 接口对应的id
+     * @param request 请求信息
+     * @return 上线结果
+     */
+    @PostMapping("/online")
+    @AuthCheck(mustRole = "admin")
+    public BaseResponse<Boolean> onlineInterface(@RequestBody IdRequest idRequest,
+                                                HttpServletRequest request) {
+        // 判断请求信息是否合法
+        if (idRequest == null || idRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Long id = idRequest.getId();
+        // 判断接口是否存在
+        InterfaceInfo oldInterfaceInfo = interfaceInfoService.getById(id);
+        if (oldInterfaceInfo == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        // 判断接口是否可以调用
+        // TODO 判断真实数据
+        com.qingmuy.muyapiclientsdk.model.User user = new com.qingmuy.muyapiclientsdk.model.User();
+        user.setUsername("test");
+        String username = muyApiClient.getUsernameByPost(user);
+        if (StringUtils.isBlank(username)) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "接口验证失败");
+        }
+        // 进本人或管理员可修改
+        InterfaceInfo interfaceInfo = new InterfaceInfo();
+        interfaceInfo.setId(id);
+        interfaceInfo.setStatus(InterfaceInfoStatusEnum.ONLINE.getValue());
+        boolean result = interfaceInfoService.updateById(interfaceInfo);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 下线接口
+     *
+     * @param idRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/offline")
+    @AuthCheck(mustRole = "admin")
+    public BaseResponse<Boolean> offlineInterface(@RequestBody IdRequest idRequest,
+                                                HttpServletRequest request) {
+        // 判断请求信息是否合法
+        if (idRequest == null || idRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Long id = idRequest.getId();
+        // 判断接口是否存在
+        InterfaceInfo oldInterfaceInfo = interfaceInfoService.getById(id);
+        if (oldInterfaceInfo == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        // 判断接口是否可以调用
+        // TODO 判断真实数据
+        com.qingmuy.muyapiclientsdk.model.User user = new com.qingmuy.muyapiclientsdk.model.User();
+        user.setUsername("test");
+        String username = muyApiClient.getUsernameByPost(user);
+        if (StringUtils.isBlank(username)) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "接口验证失败");
+        }
+        // 进本人或管理员可修改
+        InterfaceInfo interfaceInfo = new InterfaceInfo();
+        interfaceInfo.setId(id);
+        interfaceInfo.setStatus(InterfaceInfoStatusEnum.OFFLINE.getValue());
+        boolean result = interfaceInfoService.updateById(interfaceInfo);
+        return ResultUtils.success(result);
     }
 
 }
